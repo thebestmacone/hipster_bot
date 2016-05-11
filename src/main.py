64c0b393057e60c97920617244365828f28c4521
macone <thebestmacone@gmail.com>
2016-05-12 01:29:38 +0300

    Added source files

---
+++ src/main.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+
+import telebot, config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+	bot.send_message(message.chat.id, "Hello, Bro")
+
+if __name__ == "__main__":
+	bot.polling()
+	
