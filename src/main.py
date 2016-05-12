127fa3a312f7293e1453cb40140aca9eaa6f2880
macone <thebestmacone@gmail.com>
2016-05-13 01:20:42 +0300

    Added more logic for main_app

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 #coding:utf-8
 
-import telebot, config
+# import telebot, config
 
-bot = telebot.TeleBot(config.TOKEN)
+# bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=["start"])
-def start(message):
-	bot.send_message(message.chat.id, "Hello, Bro")
+# @bot.message_handler(commands=["start"])
+# def start(message):
+# 	bot.send_message(message.chat.id, "Hello, Bro")
 
-if __name__ == "__main__":
-	bot.polling()
+# if __name__ == "__main__":
+# 	bot.polling()
 	
