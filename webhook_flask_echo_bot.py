c3e0faa8f1cc9dd099f732db1281b4d5313a2752
macone <thebestmacone@gmail.com>
2016-05-13 11:15:00 +0300

    Fix

--- webhook_flask_echo_bot.py
+++ webhook_flask_echo_bot.py
@@ -78,7 +78,8 @@ def echo_message(message):
 #                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 # Start flask server
-app.run(host=WEBHOOK_LISTEN,
+if __name__ == __main__:
+    app.run(host=WEBHOOK_LISTEN,
         port=WEBHOOK_PORT)
 # ,
 #         ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
