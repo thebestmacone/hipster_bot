680c430197e81c93b8fa0302bca6987b2636d607
macone <thebestmacone@gmail.com>
2016-05-13 11:09:03 +0300

    Dirty hack

--- webhook_flask_echo_bot.py
+++ webhook_flask_echo_bot.py
@@ -15,8 +15,8 @@ WEBHOOK_HOST = 'hipstertestbot.herokuapp.com'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
 # Quick'n'dirty SSL certificate generation:
 #
@@ -71,14 +71,15 @@ def echo_message(message):
 
 
 # Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
+# bot.remove_webhook()
 
-# Set webhook
-bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
-                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+# # Set webhook
+# bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
+#                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 # Start flask server
 app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT,
-        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-        debug=True)
+        port=WEBHOOK_PORT)
+# ,
+#         ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+#         debug=True)
