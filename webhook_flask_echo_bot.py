110785df1cd749ce6a3e945fee8d78a6a944b3fe
macone <thebestmacone@gmail.com>
2016-05-13 12:43:37 +0300

    Hard fix

--- webhook_flask_echo_bot.py
+++ webhook_flask_echo_bot.py
@@ -1,99 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import telebot 
+import config 
+import os 
+from flask import Flask, request 
 
-# This is a simple echo bot using decorators and webhook with flask
-# It echoes any incoming text messages and does not use the polling method.
+bot = telebot.TeleBot(config.botToken) 
+app = Flask(__name__) 
 
-import flask
-import telebot
-import logging
-
-
-API_TOKEN = '228729068:AAEGPXDTrOO42qhu00gDGp4Iu77m14EC8ew'
-
-WEBHOOK_HOST = 'hipstertestbot.herokuapp.com'
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
-
-# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
-
-# Quick'n'dirty SSL certificate generation:
-#
-# openssl genrsa -out webhook_pkey.pem 2048
-# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
-#
-# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# with the same value in you put in WEBHOOK_HOST
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
-
-
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
-bot = telebot.TeleBot(API_TOKEN)
-
-app = flask.Flask(__name__)
-
-
-# Empty webserver index, return nothing, just http 200
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    bot.remove_webhook() 
-    bot.set_webhook(url="https://hipstertestbot.herokuapp.com/bot") 
-    return "!", 200
+@bot.message_handler(commands=['start']) 
+def start(message): 
+bot.send_message(message.chat.id, 'Привет') 
 
 @app.route("/bot", methods=['POST']) 
-def getMessage():
-    bot.process_new_messages([telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message]) 
-    return "!", 200
+def getMessage(): 
+bot.process_new_messages([telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message]) 
+return "!", 200 
 
-@app.route("/start", methods=['POST']) 
-def sendMessage():
-    bot.reply_to(message,
-                 ("Hi there, I am EchoBot.\n"
-                  "I am here to echo your kind words back to you."))
+@app.route("/") 
+def webhook(): 
+bot.remove_webhook() 
+bot.set_webhook(url="https://hipstertestbot.herokuapp.com/bot") 
+return "!", 200 
 
-# Process webhook calls
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
-def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_messages([update.message])
-        return ''
-    else:
-        flask.abort(403)
-
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 ("Hi there, I am EchoBot.\n"
-                  "I am here to echo your kind words back to you."))
-
-
-# Handle all other messages
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-# bot.remove_webhook()
-
-# # Set webhook
-# bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
-#                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
-
-# Start flask server
-if __name__ == "__main__":
-    app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT)
-    bot.polling()
-# ,
-#         ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-#         debug=True)
+port = int(os.environ.get("PORT", 5000)) 
+app.run(host='0.0.0.0', port=port) 
